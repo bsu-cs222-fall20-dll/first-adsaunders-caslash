8212459a9cd1573448ca7725356866e2bf60e7f4
Cameron <caslash@bsu.edu>
2020-09-17 15:26:05 -0400

    Fixed folders

---
+++ test/edu/bsu/cs222/firstproject/FirstProjectTest.java
@@ -0,0 +1,10 @@
+package edu.bsu.cs222.firstproject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstProjectTest {
+    @Test
+    public void Test(){
+
+    }
+}
