a701e2e970c3a7689eaff5b675b170fcef1c8f9d
adsaunders <adsaunders@bsu.edu>
2020-09-25 16:44:24 -0400

    Made it pull from wikipedia using a query

--- src/main/java/RevisionParser.java
+++ src/main/java/RevisionParser.java
@@ -3,16 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class RevisionParser {
     public static void main(String[] args) throws IOException {
-            URL oracle = new URL("https://en.wikipedia.org/wiki/Soup");
-            URLConnection yc = oracle.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(
-                    yc.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null)
-                System.out.println(inputLine);
-            in.close();
+        URL oracle = new URL("https://en.wikipedia.org/w/api.php?action=query&prop=contributors&titles=Soup&format=json");
+        URLConnection connection = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
     }
 }
