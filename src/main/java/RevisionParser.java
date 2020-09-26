a042d6d32e61edc3bacdb4efc8a56fd980f98428
adsaunders <adsaunders@bsu.edu>
2020-09-26 15:49:09 -0400

    Fixed the queries to output a limit of 20 contributers and 20 edits

--- src/main/java/RevisionParser.java
+++ src/main/java/RevisionParser.java
@@ -3,11 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.net.URLEncoder;
+//API changes: /w/api.php?action=feedrecentchanges&limit=20&titles=Soup&format=json
+//API editors: /w/api.php?action=query&prop=contributors&pclimit=20&titles=Soup&format=json
 
 public class RevisionParser {
     public static void main(String[] args) throws IOException {
-        URL oracle = new URL("https://en.wikipedia.org/w/api.php?action=query&prop=contributors&titles=Soup&format=json");
+        URL oracle = new URL("https://en.wikipedia.org/w/api.php?action=query&prop=contributors&pclimit=20&titles=Soup&format=json");
         URLConnection connection = oracle.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
